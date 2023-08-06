5e9df51a709f65a9b74624d02c6baf7b54862b4c
a7ndriyyy <andrii.nastych@gmail.com>
2023-08-06 12:30:49 +0200

    correct('

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const bodyEl = document.querySelector('body');
+const btnStartEl = document.querySelector('[data-start]');
+const btnStopEl = document.querySelector('[data-stop]');
+
+let timerId;
+btnStopEl.disabled = true;
+
+btnStartEl.addEventListener(`click`,onStartClick);
+btnStopEl.addEventListener(`click`,onStopClick);
+
+function onStartClick(){
+    onButtonActive();
+    timerId = setInterval(() => {
+        bodyEl.style.backgroundColor = getRandomHexColor();
+      }, 1000);
+}
+function onStopClick(){
+    buttonStartEl.disabled = false;
+    buttonStopEl.disabled = true;
+    clearInterval(timerId);
+}
 
