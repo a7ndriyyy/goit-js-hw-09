86a7c0c30af9b74d3197835da3ab801a37679ea1
a7ndriyyy <andrii.nastych@gmail.com>
2023-08-06 13:05:05 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -4,8 +4,23 @@ const formEl = document.querySelector('.form');
 
 formEl.addEventListener('submit', onSubmit);
 
+function onSubmit(evt) {
+  evt.preventDefault();
 
-
+  let delay = Number(evt.target.delay.value);
+  const step = Number(evt.target.step.value);
+  const amount = Number(evt.target.amount.value);
+  for (let position = 1; position <= amount; position++) {
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += step;
+  }
+}
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
